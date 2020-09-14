77be543879555d039e56bff5a86144b7d2e6ad74
bjuti001 <bjuti001@edu.xamk.fi>
2020-09-14 18:02:47 +0300

    Added attack methods for enemies
    - Enemies can now melee or shoot at player (hitscan/projectile)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,25 +7,39 @@ using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     public Animator animator;
-    public GameObject player;
+    private Transform player;
     NavMeshAgent agent;
+    public GameObject projectile;
+    private Transform projectileSpawn;
+    public GameObject healthBar;
+    public Slider healthSlider;
 
     private float health = 100f;
+    [Header("Health Settings")]
     public float maxHealth = 100f;
 
+    public enum AttackType 
+    { 
+        hitscan, 
+        projectile, 
+        melee
+    }
+
+    [Header("Attack Settings")]
+    public AttackType attackType = AttackType.hitscan;
     public float minAttackDistance = 20f;
-    public float damage = 10f;
+    private float meleeDistance = 5f;
+    public static float damage = 10f;
     public float fireRate = 1f;
     private float nextTimeToFire = 0f;
 
-    bool isAlive;
-
-    public GameObject healthBar;
-    public Slider healthSlider;
+    private bool isAlive;
 
     private void Start()
     {
         isAlive = true;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        projectileSpawn = transform.GetChild(0);
         agent = GetComponent<NavMeshAgent>();
         health = maxHealth;
         healthSlider.value = CalculateHealth();
@@ -36,29 +50,43 @@ public class Enemy : MonoBehaviour
         healthSlider.value = CalculateHealth();
 
         // Turn to look at player
-        gameObject.transform.LookAt(player.transform);
+        gameObject.transform.LookAt(player);
 
         // Get distance to player
-        float distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+        float distance = Vector3.Distance(gameObject.transform.position, player.position);
 
         // Move towards player
-        agent.SetDestination(player.transform.position);
+        agent.SetDestination(player.position);
 
-        // Stop moving when within attack range
-        if (distance <= minAttackDistance)
+        // Stop moving when within attack range (melee range or shooting range)
+        if (attackType == AttackType.melee)
         {
-            agent.isStopped = true;
+            if (distance <= meleeDistance)
+            {
+                agent.isStopped = true;
+            }
+            else if (distance > meleeDistance)
+            {
+                agent.isStopped = false;
+            }
         }
-        else if (distance > minAttackDistance)
+        else
         {
-            agent.isStopped = false;
+            if (distance <= minAttackDistance)
+            {
+                agent.isStopped = true;
+            }
+            else if (distance > minAttackDistance)
+            {
+                agent.isStopped = false;
+            }
         }
 
-        // Shoot player when within range
+        // Attack player when within range
         if (distance <= minAttackDistance && Time.time >= nextTimeToFire && isAlive)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
-            ShootPlayer();
+            AttackPlayer();
         }
 
         // Only show health bar when not at full health
@@ -81,15 +109,42 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    void ShootPlayer()
+    void AttackPlayer()
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, minAttackDistance))
+        if (attackType == AttackType.hitscan)
         {
-            if (hit.transform.CompareTag("Player"))
+            if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, minAttackDistance))
             {
-                hit.transform.GetComponent<PlayerStats>().TakeDamage(damage);
+                if (hit.transform.CompareTag("Player"))
+                {
+                    // Animation + sound
+                    hit.transform.GetComponent<PlayerStats>().TakeDamage(damage);
+                }
+            }
+        }
+        else if (attackType == AttackType.projectile)
+        {
+            // Animation + sound
+            if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, minAttackDistance))
+            {
+                if (hit.transform.CompareTag("Player"))
+                {
+                    // Animation + sound
+                    Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+                }
+            }
+        }
+        else if (attackType == AttackType.melee)
+        {
+            if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, meleeDistance))
+            {
+                if (hit.transform.CompareTag("Player"))
+                {
+                    // Animation + sound
+                    hit.transform.GetComponent<PlayerStats>().TakeDamage(damage);
+                }
             }
         }
     }
---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float lifeSpan = 2f;
+    public float projectileSpeed = 10f;
+
+    Transform player;
+    Rigidbody rb;
+    Vector3 direction;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody>();
+        direction = player.position - transform.position;
+    }
+
+    void Update()
+    {
+        rb.velocity = direction.normalized * projectileSpeed;
+        Destroy(gameObject, lifeSpan);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            collision.transform.GetComponent<PlayerStats>().TakeDamage(Enemy.damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
